f2350de6c70d9391fcb3c7c058e099898fb097b2
ankit1519 <raazankit1502@gmail.com>
2023-11-04 19:41:45 +0530

    api and redux using axios and thunk

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import { createStore,applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import axios from 'axios'
+import thunk from 'redux-thunk';
 
+//action name constant
 const increment='increment'
 const decrement='decrement'
 const incrementByAmount='incrementByAmount'
-const store=createStore(reducer,applyMiddleware(logger.default));
+
+//for db
+const init="init"
+async function getUser(dispatch,getState){
+    const {data}= await axios.get("http://localhost:3000/accounts/1");
+    dispatch({type:init,payload:data.amount})
+}
+const store=createStore(reducer,applyMiddleware(logger.default,thunk.default));
 function reducer(state={amount:1},action) {
     //immutability
     if(action.type===increment) return {amount:state.amount+1}
     if(action.type===decrement) return {amount:state.amount-1}
     if(action.type===incrementByAmount) return {amount:state.amount+action.payload}
+    if(action.type===init) return {amount:action.payload}
     return state
 }
 
@@ -19,7 +30,8 @@ function reducer(state={amount:1},action) {
 //console.log(store.getState())
 
 
-//action name constant
+
+
 
 
 const inc=()=>{
@@ -31,7 +43,13 @@ const dec=()=>{
 const iBa=(value)=>{
     return {type:incrementByAmount,payload:value}
 }
+
+const initUser=(v)=>{
+    return {type:init,payload:v}
+}
 setInterval(() => {
     //store.dispatch({type:'incrementByAmount',payload:4})
-    store.dispatch(iBa(10))
+   // store.dispatch(iBa(10))
+   //store.dispatch(initUser(200))
+   store.dispatch(getUser)
 }, 5000);
